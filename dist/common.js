cf5959dc7d74afb95638b2349df0ed377f211723
WaterWolf5918 <87424826+WaterWolf5918@users.noreply.github.com>
2024-09-25 17:04:11 -0400

    Initial commit

---
+++ dist/common.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    attributes: true,
+    childList: true,
+    subtree: true,
+    characterData: true,
+    timeout: -1,
+};
---
+++ dist/content-scripts/old/WatchRPC_full_Youtube.js
@@ -0,0 +1,9 @@
+"use strict";
+// /**
+// Get video title -> ``document.getElementsByClassName("style-scope ytd-watch-metadata").title.innerText``
+// Get video creator -> ``document.getElementById("owner").children[0].children[1].children[0].innerText``
+// Get total time -> ``document.getElementsByClassName("ytp-time-duration")[0].innerText``
+// Get current time -> ``document.getElementsByClassName("ytp-time-current")[0].innerText``
+//  */
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('\n\nIf your reading this WatchRPC for YTmusic has loaded :)\n\n');
